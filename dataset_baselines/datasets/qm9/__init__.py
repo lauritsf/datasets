39c0376e4a405531a8490e2505a090c8311cfb03
Laurits Fredsgaard Larsen <laula@dtu.dk>
2024-01-04 11:26:49 +0100

    Add tests and documentation

--- dataset_baselines/datasets/qm9/__init__.py
+++ dataset_baselines/datasets/qm9/__init__.py
@@ -1,2 +1,3 @@
+from dataset_baselines.datasets.qm9.datamodule import QM9DataModule
 
-__ALL__ = ['QM9DataModule']
+__ALL__ = ["QM9DataModule"]
---
+++ tests/datasets/test_qm9.py
@@ -0,0 +1,35 @@
+import pytest
+
+from dataset_baselines.datasets.qm9 import QM9DataModule
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("target", range(19))
+def test_get_target_stats(target):
+    dm = QM9DataModule(target=target)
+    dm.prepare_data()
+    dm.setup()
+
+    stats = dm.get_target_stats(remove_atom_refs=False, divide_by_atoms=False)
+    assert len(stats) == 3
+    mean, std, atom_refs = stats
+    # mean and std are singular tensor values
+    assert mean.shape == std.shape == ()
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("target", range(19))
+def test_datamoudle_dataloaders(target):
+    dm = QM9DataModule(target=target)
+    dm.prepare_data()
+    dm.setup()
+
+    assert dm.train_dataloader() is not None
+    assert dm.val_dataloader() is not None
+    assert dm.test_dataloader() is not None
+
+    next(iter(dm.train_dataloader()))
+    next(iter(dm.val_dataloader()))
+    next(iter(dm.test_dataloader()))
+
+
